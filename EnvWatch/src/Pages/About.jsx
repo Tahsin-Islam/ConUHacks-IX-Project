b1613d3d75eca8ef2cd25cbbed4877444c50e97b
Tahsin Islam <tahsin_i12@outlook.com>
2025-02-01 17:42:46 -0500

    added map, and css for about

--- EnvWatch/src/Pages/About.jsx
+++ EnvWatch/src/Pages/About.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './CSS/About.css'
+import people_search from '../assets/peoplesearch.png'
 
 export const About = () => {
   return (
     <div className='about'>
-        <h1>About</h1>
-        <p>EnvWatch is a web application that allows users to monitor environmental data in real-time. 
-            Users can view data from a variety of sensors and devices, and can also create custom dashboards to display the data in a way that is most useful to them. 
-            EnvWatch is designed to be easy to use and accessible to everyone, from casual users to professional researchers.
-        </p>
+        <div className='about-description'>
+            <h1>About Us</h1>
+            <p>EnvWatch is a web application that allows users to monitor environmental data in real-time. 
+                Users can view data from a variety of sensors and devices, and can also create custom dashboards to display the data in a way that is most useful to them. 
+                EnvWatch is designed to be easy to use and accessible to everyone, from casual users to professional researchers.
+            </p>
+        </div>
+        <img src={people_search} alt='placeholder' />
     </div>
   )
 }
--- EnvWatch/src/Pages/Landing.jsx
+++ EnvWatch/src/Pages/Landing.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './CSS/Landing.css';
 
 export const Landing = () => {
   return (
-    <div>
-        
+    <div className='landing'>
+      <header className="landing-header">
+        <h1>Welcome to EnvWatch</h1>
+        <p>Your go-to platform for environmental monitoring and awareness.</p>
+        <a href="#about" className="cta-button">Learn More</a>
+      </header>
     </div>
-  )
-}
+  );
+};
