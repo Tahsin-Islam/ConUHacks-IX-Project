bb0635d100990d35d28c353511b0b69b0c6357e3
Tahsin Islam <tahsin_i12@outlook.com>
2025-02-01 19:17:11 -0500

    fixed navbar css active links

--- EnvWatch/src/Components/Navbar/Navbar.jsx
+++ EnvWatch/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import './Navbar.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Navbar.css';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
-        <Link to='/' className='navlink'>
-            <div className='navlogo'>
-                <p>EnvWatch</p>
-            </div>
-        </Link>
-        <ul className='navlinks'>
-            <li>Dashboard</li>
-            <li>Insights</li>
-            <li>Data Explorer</li>
-            <Link to='/about' className='navlink'><li>About</li></Link>
-        </ul>
+      <NavLink to='/' className='navlink'>
+        <div className='navlogo'>
+          <p>EnvWatch</p>
+        </div>
+      </NavLink>
+      <ul className='navlinks'>
+        <NavLink to='/dashboard' className='navlink' activeclassname='active'><li>Dashboard</li></NavLink>
+        <NavLink to='/insights' className='navlink' activeclassname='active'><li>Insights</li></NavLink>
+        <NavLink to='/data-explorer' className='navlink' activeclassname='active'><li>Data Explorer</li></NavLink>
+        <NavLink to='/about' className='navlink' activeclassname='active'><li>About</li></NavLink>
+      </ul>
     </div>
-  )
-}
+  );
+};
